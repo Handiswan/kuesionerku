4778448b64dd6cd92f671676ea1ec88aae5419fc
Handiswan <handiswan@gmail.com>
2016-09-19 00:35:25 +0800

    semoga adajii

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer style="width:100%; height:5%; background-color:; z-index:999999">
-  <div><span>&copy; 2011-2015</span></div>
+  <div><span>&copy; 2011-2015 ok </span></div>
 </footer>
 </body>
 </html>
