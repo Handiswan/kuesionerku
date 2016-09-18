7e62cde6d4926c401bec66401d293a350feb94e7
Handiswan <handiswan@gmail.com>
2016-09-19 00:16:00 +0800

    adduuuu

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer style="width:100%; height:5%; background-color:; z-index:999999">
-  <div><span>&copy; 2015</span></div>
+  <div><span>&copy; 2011-2015</span></div>
 </footer>
 </body>
 </html>
