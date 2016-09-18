2ee1b282e356873d891bf2fd1b01d00131601163
Ismail Sudirman <ismailsudirman@skeffo.com>
2016-09-18 23:50:29 +0800

    saya edit lagi fotoernya

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer style="width:100%; height:5%; background-color:; z-index:999999">
-  <div><span>&copy; 2015 - 2016 E-Questionnaire | Edited Done</span></div>
+  <div><span>&copy; 2015 - 2016 E-Questionnaire | Edited Done | iiiiiiiiiiiiiiii</span></div>
 </footer>
 </body>
 </html>
