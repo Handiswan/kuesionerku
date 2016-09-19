7a62d6a2599d7ae4cb95dba2f5c4609af3add716
Ismail Sudirman <ismailsudirman83@gmail.com>
2016-09-19 10:05:42 +0800

    bootstrap dan jquery telah ditambahkan dalam proyek

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
-<footer style="width:100%; height:5%; background-color:; z-index:999999">
-  <div><span>&copy; 2011-2015 ok </span></div>
-</footer>
-</body>
+    <footer style="width:100%; height:5%; background-color:; z-index:999999">
+      <div><span>Copyrgiht &copy; 2016 </span></div>
+    </footer>
+  </body>
 </html>
-
-Coba
 <?php
   echo "<pre>";                                                                                                                                    //print_r($_SESSION);
 echo "</pre>";
--- header.php
+++ header.php
@@ -1,12 +1,18 @@
-<link rel="stylesheet" type="text/css" href="lib/style.css">
-<div class="header">
-	<a href="index.php"> <img src="lib/img/logo.PNG" style="padding: 10px; width:250px; margin-left:20px"> </a>
 <?php
 include "config.php";
-session_start();	
+session_start();
 ?>
+<!doctype html>
+  <html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="lib/style.css">
+      <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="lib/css/bootstrap-theme.css">
+      <script type="text/javascript" src="lib/js/jquery-3.1.0.min.js"></script>
+    </head>
+    <body>
+      <div class="header">
+	<a href="index.php"> <img src="lib/img/logo.PNG" style="padding: 10px; width:250px; margin-left:20px"> </a>
 	<a href="login.php" class="menu">Login</a>
-
-
-</div>
-<div class="backheader"></div><!--end class header--> <!--end class header-->
+      </div>
+      <div class="backheader"></div><!--end class header--> <!--end class header-->
