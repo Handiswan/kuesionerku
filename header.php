be2a1a6f880f3b1cb8fcaec6641158351e12c8cf
Ismail Sudirman <ismailsudirman83@gmail.com>
2016-09-22 20:13:13 +0800

    pembaharuan syntaxing dengan pretty pakai Atom

--- header.php
+++ header.php
@@ -1,16 +1,22 @@
 <html>
-    <head>
-      <title>Kuesioner</title>
-      <link rel="stylesheet" type="text/css" href="lib/style.css">
-      <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css">
-      <link rel="stylesheet" type="text/css" href="lib/css/bootstrap-theme.css">
-      <script src="lib/js/jquery-3.1.0.min.js"></script>
-    </head>
-    <body>
-      <div class="navbar navbar-default navbar-fixed-top"> 
-	<a href="index.php"> <img src="lib/img/logo.png" style="padding: 10px; width:250px; margin-left:20px"> </a>
-        <?php if (basename($_SERVER['SCRIPT_FILENAME']) !=  'login.php') { ?>
-	<a href="login.php" class="menu"> <button type="button" class="btn btn-default navbar-btn" style="margin-top:-6px">Login</button></a>
-        <?php } ?>
-      </div>
-      <div class="backheader"></div><!--end class header--> <!--end class header-->
+
+<head>
+  <title>Kuesioner</title>
+  <link rel="stylesheet" type="text/css" href="lib/style.css">
+  <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="lib/css/bootstrap-theme.css">
+  <script src="lib/js/jquery-3.1.0.min.js"></script>
+</head>
+
+<body>
+  <div class="navbar navbar-default navbar-fixed-top">
+    <a href="index.php"> <img src="lib/img/logo.png" style="padding: 10px; width:250px; margin-left:20px"> </a>
+    <?php if (basename($_SERVER['SCRIPT_FILENAME']) !=  'login.php') { ?>
+      <a href="login.php" class="menu">
+        <button type="button" class="btn btn-default navbar-btn" style="margin-top:-6px">Login</button>
+      </a>
+      <?php } ?>
+  </div>
+  <div class="backheader"></div>
+  <!--end class header-->
+  <!--end class header-->
--- peneliti/header.php
+++ peneliti/header.php
@@ -1,14 +1,14 @@
 <head>
-<link rel="stylesheet" type="text/css" href="../lib/style.css">
-<link rel="stylesheet" type="text/css" href="../lib/css/bootstrap.css">
-<link rel="stylesheet" href="../lib/css/bootstrap-theme.css">
-<script src="../lib/js/jquery-3.1.0.min.js"></script>
-<script src="../lib/js/kuesioner.js"></script>
-<script src="../lib/js/bootstrap.js"></script>
+  <link rel="stylesheet" type="text/css" href="../lib/style.css">
+  <link rel="stylesheet" type="text/css" href="../lib/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../lib/css/bootstrap-theme.css">
+  <script src="../lib/js/jquery-3.1.0.min.js"></script>
+  <script src="../lib/js/kuesioner.js"></script>
+  <script src="../lib/js/bootstrap.js"></script>
 </head>
-<div class="header">
-	<a href="index.php"> <img src="../lib/img/logo.png" style="padding: 10px; width:250px; margin-left:20px"> </a>
-<?php
+<div class="header" style="position:fixed;width:100%;height:100px">
+  <a href="index.php"> <img src="../lib/img/logo.png" style="padding: 10px; width:250px; margin-left:20px"> </a>
+  <?php
 include "../config.php";
 session_start();
 	if(!$_SESSION['peneliti']) {
@@ -16,34 +16,35 @@ session_start();
 	}
 ?>
 
-<script type='text/javascript'>
-	function haaa(){
-		var e = document.getElementById('menulog');
-    	if(e.style.display == 'block')
-    		e.style.display = 'none';
-    	else
-	   	e.style.display = 'block';
-		}
-		
-		function sembunyikah(){
-		document.getElementById('menulog').style.display = 'none';
-		}
-</script>
+    <script type='text/javascript'>
+      function haaa() {
+        var e = document.getElementById('menulog');
+        if (e.style.display == 'block')
+          e.style.display = 'none';
+        else
+          e.style.display = 'block';
+      }
+
+      function sembunyikah() {
+        document.getElementById('menulog').style.display = 'none';
+      }
+    </script>
 
 
 
 
-<div class="dropdown" style="margin-top:-50px; margin-left:85%">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" >
-    <?php echo $_SESSION['nama_peneliti']; ?>
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-    <li><a href="#">Profil</a></li>
-    <li role="separator" class="divider"></li>
-    <li><a href="../logout.php">Keluar</a></li>
-  </ul>
+    <div class="dropdown" style="margin-top:-50px; margin-left:85%">
+      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        <?php echo $_SESSION['nama_peneliti']; ?>
+          <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <li><a href="#">Profil</a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="../logout.php">Keluar</a></li>
+      </ul>
+    </div>
+
 </div>
-
-</div> 
-<div class="backheader"></div><!--end class header-->
+<div class="backheader"></div>
+<!--end class header-->
