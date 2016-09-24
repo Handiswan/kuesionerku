49aa3571cdca01f991dd6eaee1199764e7b4d163
Handiswan <handiswan@gmail.com>
2016-09-25 07:32:34 +0800

    scr

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 include "header.php";
 ?>
-  <div style=" background-image: url('lib/img/halaman.jpg'); height: 900px; background-attachment: fixed;">
-  </div>
-  
-  <div style="margin-left:635px; margin-top:-827px; position:fixed" >
-	<center><h4 style="text-shadow: 0 1px 1px 00AFEF; color:#FFF"> Buat kuesioner dengan mudah <br>
-  dan kumpulkan data dari responden Anda!</h4>
-  <a href="registrasi.php"> <button type="button" class="btn btn-primary">Registrasi</button></a>
-  </div>
+ <div style=" background-image: url('lib/img/halaman.jpg'); height: 900px; background-attachment: fixed; background-size:cover">
+	<div  class="row" style="margin:0">
+		<div style="float:right;width:30%;top:75px;position:relative">
+			<h4 style="text-shadow: 0 1px 1px 00AFEF; color:#FFF"> Buat kuesioner dengan mudah dan kumpulkan data dari responden Anda!</h4>
+			<a href="registrasi.php"> <button type="button" class="btn btn-primary">Registrasi</button></a>
+		</div>
+	</div>
+</div>
  <?php include "footer.php"; ?>
