e3dbb42cf7b2198e25f04a01732a4cdae1c5e63f
Ismail Sudirman <ismailsudirman83@gmail.com>
2016-09-25 17:51:00 +0800

    kuesioner peneliti tahap 2 insert kuesioner q_gutman tabel berhasil

--- ajax/buat_kuesioner.php
+++ ajax/buat_kuesioner.php
@@ -45,18 +45,75 @@ switch($_POST['skala']) {
 		           }
 		        }
 
-			$hasil = json_encode(array(
-			 			'hasil' => 'ok',
-						'jenis_skala' => $_POST['skala'],
-						'last_id_skala' => $last_id_skala,
-						'id_kuesioner' => $last_id,
+			      $hasil = json_encode(array(
+			 			  'hasil' => 'ok',
+						  'jenis_skala' => $_POST['skala'],
+						  'last_id_skala' => $last_id_skala,
+						  'id_kuesioner' => $last_id,
 						));
-			echo $hasil;
-		        break;
+			      echo $hasil;
+	       }
+      }
+      break;
+      case "guttman":
+        $sv = $PDO->prepare(
+          "INSERT INTO kuesioner(
+            judul_penelitian,
+            keterangan,
+            jenis_skala, tanggal, url) VALUES (?, ?, ?, NOW(), ?)"
+        );
+        // jika persiapan aman ...
+        if ($sv) {
+          echo "persiapan eksekusi berhasil";
+          // lakukan tambatan data
+          $sv->bindParam(1, $_POST['judul']);
+          $sv->bindParam(2, $_POST['keterangan']);
+          $sv->bindParam(3, $_POST['skala']);
+          $sv->bindParam(4, generateRandomString());
+          // kemudian lakukan pengujian eksukusi
+          if ($sv->execute()) {
+            // dapatkan id terakhir yang baru saja ditambahkan
+            $id_terakhir = $PDO->lastInsertId(); // untuk digunakan pada q_gutman
+            // tambah data pada tabel q_gutman
+            $q = $PDO->prepare("INSERT INTO q_gutman(kuesioner_id) VALUES (?)");
+            if ($q) {
+              $q->bindParam(1, $id_terakhir);
+              $q->execute();
+              $id_skala_terakhir = $PDO->lastInsertId();
+              // berikutnya perbaharui tabel kuesioner
+              $u = $PDO->prepare(
+                "UPDATE kuesioner
+                 SET id_peneliti = ?, id_skala = ? WHERE id_kuesioner = ?
+                 "
+              );
+              // jika persiapan ok ...
+              if ($u) {
+                // lakukan tambatan data
+                $u->bindParam(1, $_SESSION['id_peneliti']);
+                $u->bindParam(2, $id_skala_terakhir);
+                $u->bindParam(3, $i_terakhir);
+                $u->execute();
+              }
+            }
 
-	    } else {
-		echo "GAGAL menyimpan DATA";
-	    }
-	}
-}
+            $hasil = json_encode(array(
+                'hasil' => 'ok',
+                'jenis_skala' => $_POST['skala'],
+                'id_skala_terakhir' => $id_skala_terakhir,
+                'id_kuesioner' => $id_terakhir,
+            ));
+            echo $hasil;
+          }
+        } else {
+          echo "persiapan eksekusi gagal";
+        }
+        echo "Data guttman";
+      break;
+
+      case "rating":
+      break;
+
+      case "semantic":
+      break;
+    }
 ?>
