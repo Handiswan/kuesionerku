24905b63c6f41cfb3d5a2d2dd63bde33f505dbad
Ismail Sudirman <ismailsudirman83@gmail.com>
2016-09-26 10:38:12 +0800

    berkas kuesioner.sql 26 Sep 2016

--- kuesioner.sql
+++ kuesioner.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 24, 2016 at 04:14 
+-- Generation Time: Sep 26, 2016 at 04:36 
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -70,7 +70,9 @@ CREATE TABLE `form_isian` (
 INSERT INTO `form_isian` (`id`, `kuesioner_id`, `keterangan`, `tipe`) VALUES
 (64, 334, 'Nama', 'text'),
 (65, 334, 'Usia', 'angka'),
-(66, 334, 'Jenis Kelamin', 'gender');
+(66, 334, 'Jenis Kelamin', 'gender'),
+(67, 354, 'Nama', 'text'),
+(68, 354, 'Usia', 'text');
 
 -- --------------------------------------------------------
 
@@ -94,7 +96,23 @@ CREATE TABLE `kuesioner` (
 --
 
 INSERT INTO `kuesioner` (`id_kuesioner`, `jenis_skala`, `id_skala`, `id_peneliti`, `judul_penelitian`, `keterangan`, `tanggal`, `url`) VALUES
-(334, 'likert', 262, 9, 'Pengaruh cinta dan kasih sayang terhadap perilaku menyimpang anak muda kota Makassar', 'Penelitian ini menggunakan pendekatan kasih dan sayang kepada 500 pemuda kota Makassar yang terserang virus cinta.', '2016-09-24', 'L5QYhxYktG');
+(334, 'likert', 262, 9, 'Pengaruh cinta dan kasih sayang terhadap perilaku menyimpang anak muda kota Makassar', 'Penelitian ini menggunakan pendekatan kasih dan sayang kepada 500 pemuda kota Makassar yang terserang virus cinta.', '2016-09-24', 'L5QYhxYktG'),
+(346, 'guttman', 0, 0, 'untuk guttman', 'jfio', '2016-09-25', 'QiqM5CP7Yk'),
+(347, 'guttman', 0, 0, 'Iji', 'Keterangan diisi disini...', '2016-09-25', '1jLsq9eZVf'),
+(348, 'guttman', 0, 0, 'fe', 'Keterangan diisi disini...', '2016-09-25', 'jM1nYw0wC3'),
+(349, 'guttman', 0, 0, 'JJi', 'Keterangan diisi disini...', '2016-09-25', '2GUqAdXGBy'),
+(350, 'guttman', 0, 0, 'Ijioj', 'Keterangan diisi disini...', '2016-09-25', '5KoCDpRQEC'),
+(351, 'guttman', 0, 0, 'jio', 'Keterangan diisi disini...', '2016-09-25', 'fwR1bwjlaS'),
+(353, 'guttman', 0, 0, 'guttman', 'Keterangan diisi disini...', '2016-09-25', 'HgxZ8DrJs7'),
+(355, 'guttman', 0, 0, 'jfeioj', 'Keterangan diisi disini...', '2016-09-25', 'xdzFwEL4A4'),
+(356, 'guttman', 0, 0, 'Ismail Sudirman', 'Keterangan diisi disini...', '2016-09-25', 'z2VHxqXYMR'),
+(357, 'guttman', 0, 0, 'Halo', 'Keterangan diisi disini...', '2016-09-25', 'H4fWgwvm7f'),
+(358, 'guttman', 0, 0, 'Helo', 'Keterangan diisi disini...', '2016-09-25', 'yKG0yTHdQv'),
+(359, 'guttman', 0, 0, 'Data baru yang banyak', 'Keterangan diisi disini...', '2016-09-25', 'JpF7gESE3h'),
+(360, 'guttman', 0, 0, 'Linux', 'joij Keterangan diisi disini...', '2016-09-25', 'UDJWBlTokC'),
+(361, 'guttman', 0, 0, 'Linux', 'Keterangan diisi disini...', '2016-09-25', '8WR2eKFeTG'),
+(362, 'guttman', 0, 0, 'Linux', 'Keterangan diisi disini...', '2016-09-25', 'SB13OAAJsH'),
+(363, 'guttman', 0, 0, 'Keren...', 'Keterangan diisi disini...', '2016-09-25', 'l8fQEI5yBs');
 
 -- --------------------------------------------------------
 
@@ -132,8 +150,31 @@ INSERT INTO `peneliti` (`id_peneliti`, `nama_peneliti`, `email`, `password`, `tt
 --
 
 CREATE TABLE `q_gutman` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `kuesioner_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `q_gutman`
+--
+
+INSERT INTO `q_gutman` (`id`, `kuesioner_id`) VALUES
+(1, 346),
+(2, 347),
+(3, 348),
+(4, 349),
+(5, 350),
+(6, 351),
+(7, 353),
+(8, 355),
+(9, 356),
+(10, 357),
+(11, 358),
+(12, 359),
+(13, 360),
+(14, 361),
+(15, 362),
+(16, 363);
 
 -- --------------------------------------------------------
 
@@ -148,6 +189,13 @@ CREATE TABLE `q_gutman_pilihan_f` (
   `q_gutman_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `q_gutman_pilihan_f`
+--
+
+INSERT INTO `q_gutman_pilihan_f` (`id`, `nilai`, `keterangan`, `q_gutman_id`) VALUES
+(1, 89, 'Jijoi', 16);
+
 -- --------------------------------------------------------
 
 --
@@ -160,6 +208,13 @@ CREATE TABLE `q_gutman_pilihan_uf` (
   `nilai` int(11) NOT NULL,
   `keterangan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `q_gutman_pilihan_uf`
+--
+
+INSERT INTO `q_gutman_pilihan_uf` (`id`, `q_gutman_id`, `nilai`, `keterangan`) VALUES
+(1, 16, 89, 'Jijoi');
 
 -- --------------------------------------------------------
 
@@ -177,7 +232,20 @@ CREATE TABLE `q_liker` (
 --
 
 INSERT INTO `q_liker` (`id_liker`, `kuesioner_id`) VALUES
-(262, 334);
+(262, 334),
+(263, 335),
+(264, 336),
+(265, 337),
+(266, 338),
+(267, 339),
+(268, 340),
+(269, 341),
+(270, 342),
+(271, 343),
+(272, 344),
+(273, 345),
+(274, 352),
+(275, 354);
 
 -- --------------------------------------------------------
 
@@ -200,7 +268,16 @@ INSERT INTO `q_liker_pilihan_f` (`id`, `q_liker_id`, `nilai`, `keterangan`) VALU
 (203, 262, 4, 'Sangat Sesuai'),
 (204, 262, 3, 'Sesuai'),
 (205, 262, 2, 'Tidak Sesuai'),
-(206, 262, 1, 'Sangat Tidak Sesuai');
+(206, 262, 1, 'Sangat Tidak Sesuai'),
+(207, 263, 88, 'Ijieo'),
+(208, 263, 8, 'jioj'),
+(209, 263, 88, 'ejwoij'),
+(210, 264, 88, '3'),
+(211, 265, 88, 'jioj'),
+(212, 265, 8, 'i8j'),
+(213, 265, 89998, '888'),
+(214, 275, 8, 'jioj'),
+(215, 275, 89, 'Iki');
 
 -- --------------------------------------------------------
 
@@ -223,7 +300,16 @@ INSERT INTO `q_liker_pilihan_uf` (`id`, `q_liker_id`, `nilai`, `keterangan`) VAL
 (203, 262, 1, 'Sangat Sesuai'),
 (204, 262, 2, 'Sesuai'),
 (205, 262, 3, 'Tidak Sesuai'),
-(206, 262, 4, 'Sangat Tidak Sesuai');
+(206, 262, 4, 'Sangat Tidak Sesuai'),
+(207, 263, 88, 'Ijieo'),
+(208, 263, 8, 'jioj'),
+(209, 263, 88, 'ejwoij'),
+(210, 264, 88, '3'),
+(211, 265, 89998, 'jioj'),
+(212, 265, 8, 'i8j'),
+(213, 265, 88, '888'),
+(214, 275, 89, 'jioj'),
+(215, 275, 8, 'Iki');
 
 -- --------------------------------------------------------
 
@@ -234,7 +320,8 @@ INSERT INTO `q_liker_pilihan_uf` (`id`, `q_liker_id`, `nilai`, `keterangan`) VAL
 CREATE TABLE `q_rating` (
   `id` int(11) NOT NULL,
   `nilai_min` int(11) NOT NULL,
-  `nilai_max` int(11) NOT NULL
+  `nilai_max` int(11) NOT NULL,
+  `kuesioner_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -248,7 +335,8 @@ CREATE TABLE `q_semantik` (
   `nilai_min` int(11) NOT NULL,
   `nilai_max` int(11) NOT NULL,
   `label_min` varchar(100) NOT NULL,
-  `label_max` int(11) NOT NULL
+  `label_max` int(11) NOT NULL,
+  `kuesioner_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -299,7 +387,10 @@ INSERT INTO `soal` (`id_soal`, `id_responden`, `id_kuesioner`, `no_soal`, `perta
 (91, 0, 334, 0, 'Saya merasa nyaman berkenalan dengan sahabat baru karena kenyamanan darinya', 'f'),
 (92, 0, 334, 0, 'Saya merasa tidak nyaman berkenaan dengan mengenal kenalan baru karena dia merasa tidak aman', 'uf'),
 (93, 0, 334, 0, 'Hati saya layu tersipu malu seperti kayu dari sagu bila bertemu dengan orang lugu bawa sapu', 'f'),
-(94, 0, 334, 0, 'Hati saya beku seperti abu vulkanik ketika merayu orang lugu yang tersipu malu tapi mau', 'uf');
+(94, 0, 334, 0, 'Hati saya beku seperti abu vulkanik ketika merayu orang lugu yang tersipu malu tapi mau', 'uf'),
+(95, 0, 354, 0, 'joijfe ', 'uf'),
+(96, 0, 354, 0, 'Data joij f  weffe ', 'f'),
+(97, 0, 354, 0, 'ff', 'uf');
 
 --
 -- Indexes for dumped tables
@@ -408,42 +499,47 @@ ALTER TABLE `data`
 -- AUTO_INCREMENT for table `form_isian`
 --
 ALTER TABLE `form_isian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 --
 -- AUTO_INCREMENT for table `kuesioner`
 --
 ALTER TABLE `kuesioner`
-  MODIFY `id_kuesioner` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=335;
+  MODIFY `id_kuesioner` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=364;
 --
 -- AUTO_INCREMENT for table `peneliti`
 --
 ALTER TABLE `peneliti`
   MODIFY `id_peneliti` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
+-- AUTO_INCREMENT for table `q_gutman`
+--
+ALTER TABLE `q_gutman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
 -- AUTO_INCREMENT for table `q_gutman_pilihan_f`
 --
 ALTER TABLE `q_gutman_pilihan_f`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `q_gutman_pilihan_uf`
 --
 ALTER TABLE `q_gutman_pilihan_uf`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `q_liker`
 --
 ALTER TABLE `q_liker`
-  MODIFY `id_liker` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=263;
+  MODIFY `id_liker` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=276;
 --
 -- AUTO_INCREMENT for table `q_liker_pilihan_f`
 --
 ALTER TABLE `q_liker_pilihan_f`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=207;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=216;
 --
 -- AUTO_INCREMENT for table `q_liker_pilihan_uf`
 --
 ALTER TABLE `q_liker_pilihan_uf`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=207;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=216;
 --
 -- AUTO_INCREMENT for table `q_rating`
 --
@@ -468,7 +564,7 @@ ALTER TABLE `responden_info`
 -- AUTO_INCREMENT for table `soal`
 --
 ALTER TABLE `soal`
-  MODIFY `id_soal` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+  MODIFY `id_soal` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
